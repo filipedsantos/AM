1a8582aa0166105cb5eff099b98755658acf3339
Filipe Santos <fd.filipe@gmail.com>
2016-12-20 01:17:56 +0000

    Add SnakeGame.class and SnakeView.class

--- SnakeMultiplayer/app/src/main/java/pt/isec/a21180878/snakemultiplayer/MainActivity.java
+++ SnakeMultiplayer/app/src/main/java/pt/isec/a21180878/snakemultiplayer/MainActivity.java
@@ -1,7 +1,11 @@
 package pt.isec.a21180878.snakemultiplayer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import pt.isec.a21180878.snakemultiplayer.game.SnakeGame;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onStartGame(View view) {
+        Intent intent = new Intent(MainActivity.this, SnakeGame.class);
+        startActivity(intent);
     }
 }
